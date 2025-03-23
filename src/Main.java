1273d0ee2cd4c746af38c89236e0b3549e1dd9d3
PC <nikhiliet10@gmail.com>
2025-03-23 18:32:13 +0530

    code commit 2

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,8 @@ public class Main {
         System.out.println("hello Nikhil Shukla");
         for(int i =0; i< 15; i++){
             System.out.println("value of i...."+i);
+            System.out.println("value of i...."+i);
+
         }
     }
 }
