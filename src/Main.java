120309a0f565785156e2f2a23cb9559cbdf32e2c
PC <nikhiliet10@gmail.com>
2025-03-23 18:08:58 +0530

    code commit 1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("hello Nikhil Shukla");
+        for(int i =0; i< 15; i++){
+            System.out.println("value of i...."+i);
+        }
     }
 }
